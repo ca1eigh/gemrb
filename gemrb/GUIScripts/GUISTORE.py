232fe510a9bbee4a088fd6eec71c2ee1ba0edeb2
Brad Allred <bradallred@me.com>
2023-08-02 17:17:22 -0600

    remove IE_GUI_BUTTON_PLAYALWAYS
    "play always" is the default, and the only buttons that differ are the clocks
    borrow the unused A_ANI_NO_SHADOW bit

--- gemrb/GUIScripts/GUISTORE.py
+++ gemrb/GUIScripts/GUISTORE.py
@@ -1852,7 +1852,6 @@ def DonateGold ():
 	TextArea.SetFlags (IE_GUI_TEXTAREA_AUTOSCROLL)
 
 	Button = Window.GetControl (10)
-	Button.SetFlags (IE_GUI_BUTTON_PLAYALWAYS, OP_OR)
 	if Button:
 		Button.SetAnimation ("DONATE")
 
